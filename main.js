e9c182213090324de22603b4e9fffb721cc16478
TheSquiddyLink <preston@happychaps.com>
2024-02-03 11:01:30 -0700

    Added Comments

--- main.js
+++ main.js
@@ -4,14 +4,21 @@ window.onload = start()
 
 async function start() {
 
+    // This gets all of the default elements that are used
     let el = getElements()
     
+    // This gets a random image, and requires for an image, name, handle, and icon HTML element
     randomImage(el.img, el.name, el.handle, el.icon)
+    // This is a forever loop
     while(true){
+        // This tells the program to sleep (wait) for 5 seconds
         await sleep(5)
-        animation([el.img], "fadeIn", 2)
+        // The first animation(s) is the disapear animation
+        animation([el.img], "shrink", 2)
         animation([el.name, el.handle, el.icon], 'fadeOut', 3)
+        // Code will not wait for animation to finish, so a sleep is needed
         await sleep(2)
+        // The last animations are the appear animation
         randomImage(el.img, el.name, el.handle, el.icon)
         animation([el.img], 'grow', 2)
         animation([el.name, el.handle, el.icon], 'fadeIn', 3)
